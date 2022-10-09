6d339ea28276b141b940b39d227e1a8a913a13d7
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-10-09 23:14:04 +0300

    Add UD, use restful in routes, change links, add before_action add set_project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,66 @@
 class TasksController < ApplicationController
+  # set_task - приватная функция в данном классе
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     # Task.all появляются с модели (при наследовании от ApplicationRecord)
     @tasks = Task.all
+    @project = Project.find(params[:project_id])
+  end
+
+  def show
   end
 
   # здесь используется @task за место @tasks, так как
   def new
     @task = Task.new
+    @project = Project.new
+  end
+
+  def edit
   end
 
   def create
     @task = Task.new(task_params)
+    # блин круто, менять параметр можно в самом контроле
+    @task.deadline_at = 7.days.after
 
     if @task.save
-      redirect_to @task
+      # project_task_path(task)
+      redirect_to project_tasks_path
     else
       render :new
     end
   end
 
-  def show
-    @task = Task.find(params[:id])
+  def update
+    if @task.update(task_params)
+      redirect_to project_tasks_path, notice: "Task was successfully created."
+    else
+      # флэш тоже не срабатывает
+      flash.now[:task_notice] = "Something went wrong. Try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to projects_url, notice: "Task was successfully destroyed."
   end
 
   private
+
+  def set_task
+    # можно выводить данные
+    # p "hello there"
+    # p params[:project_id]
+    # find_by ищет первое совпадение
+    # find_by bang version is find_by!
+    # Task.find_by!(id: params[:id]) равнозначно Task.find(id: params[:id])
+    @task = Task.find_by!(id: params[:id])
+    @project = Project.find(params[:project_id])
+    # @project = @task.project
+  end
 
   def task_params
     params.require(:task).permit(:title, :description, :deadline_at, :project_id)
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 # эта штука по-другому называется ActiveRecord
 class Task < ApplicationRecord
+  validates :title, presence: true
+  # все манипулюции над параметрами модели должны выполняться внутри модели
+  # validates :deadline_at, deadline_at < created_at
   belongs_to :project
 end
