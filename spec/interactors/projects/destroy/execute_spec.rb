b75342c5d3bab8c7efd255059922d9c3128d6d57
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-11-21 13:35:12 +0300

    do send_notification interactor

--- spec/interactors/projects/destroy/execute_spec.rb
+++ spec/interactors/projects/destroy/execute_spec.rb
@@ -1,1 +1,19 @@
+require "rails_helper"
 
+describe Projects::Destroy::Execute do
+  describe ".call" do
+    let(:interactor) { described_class.new(project: project) }
+
+    context "when params are valid" do
+      let(:project) { create :project }
+      binding.pry
+      it { expect { interactor.run }.to change(Project, :count).by(1) }
+    end
+
+    context "when params are invalid" do
+      let(:project) { nil }
+
+      let(:expected_error_message) { "Invalid data" }
+    end
+  end
+end
