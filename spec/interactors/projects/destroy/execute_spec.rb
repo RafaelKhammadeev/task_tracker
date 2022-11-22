b53b164dc1825c72687c38daf06339078eef6bcb
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-11-22 11:20:36 +0300

    do text on destroy_execute

--- spec/interactors/projects/destroy/execute_spec.rb
+++ spec/interactors/projects/destroy/execute_spec.rb
@@ -1,1 +1,26 @@
+require "rails_helper"
 
+describe Projects::Destroy::Execute do
+  describe ".call" do
+    let(:interactor) { described_class.new(project: project) }
+
+    context "when params are valid" do
+      let(:project) { create :project }
+
+      it { expect { interactor.run }.to change(Project.all, :count).by(-1) }
+    end
+
+    context "when params are invalid" do
+      let(:project) { nil }
+
+      let(:expected_error_message) { "Invalid Data" }
+
+      it "fails" do
+        interactor.run
+
+        expect(Project.exists?(project.id)).to be false
+        expect(interactor.context.error).to eq(expected_error_message)
+      end
+    end
+  end
+end
