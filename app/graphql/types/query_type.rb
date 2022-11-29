95af534fdda2a3999df98cbb9694a60a611c1fea
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-11-29 18:34:30 +0300

    fix mistakes

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,5 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
-
     field :project, resolver: Resolvers::Project
     field :projects, resolver: Resolvers::Projects
     field :task, resolver: Resolvers::Task
