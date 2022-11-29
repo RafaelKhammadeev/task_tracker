3b39b7bb9e93143167fbb4671af44a7c24f6fe75
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-11-29 18:42:24 +0300

    delete field project_id from task_type

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -4,7 +4,6 @@ module Types
     field :title, String
     field :description, String
     field :deadline_at, GraphQL::Types::ISO8601DateTime
-    field :project_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :state, String, null: false
