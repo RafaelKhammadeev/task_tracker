4326e77a75a7a30ded8bf4fe7b94d9984e49a7af
RafaelKhammadeev <94076749+RafaelKhammadeev@users.noreply.github.com>
2022-12-12 21:59:16 +0300

    Update app/graphql/mutations/create_task.rb
    Co-authored-by: Rasim Khusaenov <rasim.khusaenov@stackers.team>

--- app/graphql/mutations/create_task.rb
+++ app/graphql/mutations/create_task.rb
@@ -11,11 +11,7 @@ module Mutations
       project = Project.find(input.project_id)
       result = Tasks::Create.call(task_params: input.to_h, project: project)
 
-      if result.success?
-        result.to_h
-      else
-        result.to_h.merge(errors: formatted_errors(result.task))
-      end
+      result.to_h.merge(errors: formatted_errors(result.task))
     end
   end
 end
