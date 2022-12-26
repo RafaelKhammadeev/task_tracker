98e4cd01f5b3adf3d8a9c82d5f8fa2fe861fe834
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-12-26 16:29:07 +0300

    commit

--- app/graphql/types/payloads/create_comment_payload.rb
+++ app/graphql/types/payloads/create_comment_payload.rb
@@ -1,7 +1,7 @@
 module Types
   module Payloads
     class CreateCommentPayload < Types::BaseObject
-      field :comment, Types::CommentType, null: true
+      field :comment, Types::CommentType, null: false
       field :errors, [Types::UserError], null: false
     end
   end
