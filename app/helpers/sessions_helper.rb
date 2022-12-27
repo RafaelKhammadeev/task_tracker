e7b3b00b970132a12eb752fe63590ea5f55d0b85
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-12-27 23:00:40 +0300

    fix rubocop

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,10 @@ module SessionsHelper
   end
 
   def check_for_null(text)
-    text.blank? ? nil : text
+    text.presence
   end
 
   def check_name_output
-    check_for_null(current_user['first_name']) || check_for_null(current_user['last_name']) || current_user['email']
+    check_for_null(current_user["first_name"]) || check_for_null(current_user["last_name"]) || current_user["email"]
   end
 end
