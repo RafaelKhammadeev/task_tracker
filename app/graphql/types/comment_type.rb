acae1dd6471b5781fb6b2b7b41579360b4b297f9
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-12-31 11:03:47 +0300

    fix after review 2.1

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -3,9 +3,8 @@ module Types
     field :id, ID, null: true
     field :content, String
     field :task_id, Integer, null: false
-    field :user_id, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
     field :task, TaskType, null: false
   end
 end
