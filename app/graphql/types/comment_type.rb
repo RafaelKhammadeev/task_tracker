640bcc41bfc522cd92d27c575e8fd0bff863c8bd
RafaelKhammadeev <msmk.swimmer@gmail.com>
2022-12-31 11:19:15 +0300

    little change

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -2,8 +2,8 @@ module Types
   class CommentType < Types::BaseObject
     field :id, ID, null: true
     field :content, String
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: true
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime
+    field :updated_at, GraphQL::Types::ISO8601DateTime
     field :task, TaskType, null: false
   end
 end
