4c836d221bca1c95cdea35bdfbe5532e9afc661f
RafaelKhammadeev <msmk.swimmer@gmail.com>
2023-01-03 11:33:12 +0300

    fix after review 2.3

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -1,10 +1,9 @@
 module Types
   class TaskType < Types::BaseObject
-    field :id, ID, null: false
+    field :id, ID
     field :title, String
     field :description, String
     field :deadline_at, GraphQL::Types::ISO8601DateTime
-    field :project_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime
     field :updated_at, GraphQL::Types::ISO8601DateTime
     field :state, String
